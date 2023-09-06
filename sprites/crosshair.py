932ad3e7f8e81c001ef72aaf692910d5e30420e2
mcastre1 <miguelcastrejongal@gmail.com>
2023-09-06 06:41:23 -0600

    Crosshair added on mouse position

--- sprites/crosshair.py
+++ sprites/crosshair.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Crosshair(pygame.sprite.Sprite):
-    def __init__(self ,width ,height ,pos_x, pos_y, color):
+    def __init__(self, picture_path):
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = pygame.image.load(picture_path)
         self.rect = self.image.get_rect()
-        self.rect.center = (pos_x, pos_y)
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
--- sprites/sprites.py
+++ sprites/sprites.py
@@ -9,13 +9,16 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
-background = pygame.image.load('./graphics/BG.png')
+background = pygame.image.load('./sprites/graphics/BG.png')
 
 
-crosshair = Crosshair(50,50,100,100,(255,255,255))
+crosshair = Crosshair('./sprites/graphics/crosshair.png')
 
 crosshair_group = pygame.sprite.Group()
 crosshair_group.add(crosshair)
+
+pygame.mouse.set_visible(False)
+
 
 while True:
     for event in pygame.event.get():
@@ -25,7 +28,9 @@ while True:
 
 
     pygame.display.flip()
-    screen.blit(background,(0,0))
+    #screen.blit(background,(0,0))
+    screen.fill((255,255,255))
     crosshair_group.draw(screen)
+    crosshair_group.update()
     clock.tick(60)
             
