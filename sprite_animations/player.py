a3286f9bef172017905caa001702d22f9d1177d4
mcastre1 <miguelcastrejongal@gmail.com>
2023-09-08 21:55:18 -0600

    Animations

--- sprite_animations/player.py
+++ sprite_animations/player.py
@@ -4,7 +4,11 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
 
-        self.image = pygame.Surface([20, 20])
-        self.image.fill((255,255,255))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = [pos_x, pos_y]
+        # self.image = pygame.Surface([20, 20])
+        # self.image.fill((255,255,255))
+        # self.rect = self.image.get_rect()
+        # self.rect.topleft = [pos_x, pos_y]
+
+        self.sprites = []
+        for n in range(1,11):
+            self.sprites.append(pygame.image.load(f'./graphics/attack_{n}'))
