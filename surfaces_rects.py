2aa33ecf708eb9be9b453fdc93b5ffe949fd82df
mcastre1 <miguelcastrejongal@gmail.com>
2023-09-11 19:49:31 -0600

    You can move surfaces or images by moving the rect from get_rect and blitting the image or surface onto the rect.

--- surfaces_rects.py
+++ surfaces_rects.py
@@ -7,7 +7,12 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((800,800))
 
 second_surface = pygame.Surface([100,200])
+second_surface_rect = second_surface.get_rect()
 second_surface.fill((0,0,255))
+
+kitten = pygame.image.load('kitten.jpg')
+kitten_rect = kitten.get_rect()
+kitten_rect.topleft = [100,100]
 
 while True:
     for event in pygame.event.get():
@@ -17,5 +22,9 @@ while True:
 
     screen.fill((255,255,255))
     screen.blit(second_surface, (0,50))
+    screen.blit(kitten, kitten_rect)
+    kitten_rect.right += 3
+
+
     pygame.display.flip() # Draws everything in this loop cycle.
     clock.tick(60) # Frame rate, in this case 60 frames per second (FPS)
