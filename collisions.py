bf9644203d1883a190ac40f1500888f1539fa741
mcastre1 <miguelcastrejongal@gmail.com>
2023-09-12 21:35:42 -0600

    Directional collision

--- collisions.py
+++ collisions.py
@@ -3,7 +3,7 @@ import pygame
 import sys
 
 def bouncing_rect():
-    global x_speed, y_speed
+    global x_speed, y_speed, other_speed
     moving_rect.x += x_speed
     moving_rect.y += y_speed
 
@@ -13,16 +13,21 @@ def bouncing_rect():
     if moving_rect.bottom >= screen_height or moving_rect.top <= 0:
         y_speed *= -1
 
+    # moving the other rectangle
+    other_rect.y += other_speed
+    if other_rect.top <= 0 or other_rect.bottom >= screen_height:
+        other_speed *= -1
+
     # collision with other rect
     collision_tolerance = 10
     if moving_rect.colliderect(other_rect):
-        if abs(other_rect.top - moving_rect.bottom) < collision_tolerance:
+        if abs(other_rect.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:
             y_speed *= -1
-        if abs(other_rect.bottom - moving_rect.top) < collision_tolerance:
+        if abs(other_rect.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
             y_speed *= -1
-        if abs(other_rect.right - moving_rect.left) < collision_tolerance:
+        if abs(other_rect.right - moving_rect.left) < collision_tolerance and x_speed < 0:
             x_speed *= -1
-        if abs(other_rect.left - moving_rect.right) < collision_tolerance:
+        if abs(other_rect.left - moving_rect.right) < collision_tolerance and x_speed > 0:
             x_speed *= -1
 
 
