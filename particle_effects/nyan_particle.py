42e86c581a6d58d6b9ee37efb31bff8cc01ea7b7
mcastre1 <miguelcastrejongal@gmail.com>
2023-09-14 21:14:47 -0600

    Created left moving rects as particles, delete when x position is less than 0

---
+++ particle_effects/nyan_particle.py
@@ -0,0 +1,32 @@
+import pygame, sys, random
+
+class NyanParticlePrinciple:
+    def __init__(self):
+        self.particles = []
+        self.screen = pygame.display.get_surface()
+
+    # moves and draws particles
+    def emit(self):
+        if self.particles:
+            self.delete_particles()
+            for particle in self.particles:
+                # move
+                particle[0][0] -= 1
+
+                # draw a cirlce around the particle
+                #pygame.draw.circle(self.screen, pygame.Color('white'), particle[0], int(particle[1]))
+                rect = pygame.Rect(particle[0][0], particle[0][1], 10, 10)
+                pygame.draw.rect(self.screen, pygame.Color('red'), rect)
+    # adds particles
+    def add_particles(self, offset):
+        pos_x = pygame.mouse.get_pos()[0]
+        pos_y = pygame.mouse.get_pos()[1]
+
+        particle_rect = [[pos_x, pos_y]]
+        self.particles.append(particle_rect)
+
+    # delete particles after a certain time
+    def delete_particles(self):
+        particle_copy = [particle for particle in self.particles if particle[0][0] > 0]
+        self.particles = particle_copy
+        print(len(self.particles))
--- particle_effects/particle_effects.py
+++ particle_effects/particle_effects.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from particlePrinciple import ParticlePrinciple
+from nyan_particle import NyanParticlePrinciple
 
 screen_width = 800
 screen_height = 800
@@ -9,10 +10,11 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width,screen_height))
 
 particle1 = ParticlePrinciple()
+particle2 = NyanParticlePrinciple()
 
 # This is called every 40 milliseconds
 PARTICLE_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(PARTICLE_EVENT, 150)
+pygame.time.set_timer(PARTICLE_EVENT, 30)
 
 while True:
     for event in pygame.event.get():
@@ -20,9 +22,11 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == PARTICLE_EVENT:
-            particle1.add_particles()
+            #particle1.add_particles()
+            particle2.add_particles(6)
 
     screen.fill((30,30,30))
-    particle1.emit()
+    #particle1.emit()
+    particle2.emit()
     pygame.display.flip()
     clock.tick(60)
